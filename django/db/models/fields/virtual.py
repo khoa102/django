9ce4cf407c6f90384b352b68625fcacc40672b95
Michal Petrucha <michal.petrucha@ksp.sk>
2013-08-05 19:37:44 +0200

    Initial implementation of CompositeField.

--- django/db/models/fields/virtual.py
+++ django/db/models/fields/virtual.py
@@ -1,6 +1,16 @@
 from __future__ import unicode_literals
 
+from collections import namedtuple
+
+from django.db.models import signals
 from django.db.models.fields import Field
+from django.utils.encoding import (force_text, python_2_unicode_compatible,
+        quote, unquote)
+from django.utils import six
+
+
+COMPOSITE_VALUE_SEPARATOR = ','
+COMPOSITE_VALUE_QUOTING_CHAR = '~'
 
 
 class VirtualField(Field):
@@ -40,3 +50,69 @@ class VirtualField(Field):
 
     def __set__(self, instance, value):
         pass
+
+
+class CompositeField(VirtualField):
+    """
+    Virtual field type enclosing several atomic fields into one.
+    """
+    def __init__(self, *fields, **kwargs):
+        self.fields = fields
+        super(CompositeField, self).__init__(**kwargs)
+
+    def contribute_to_class(self, cls, name):
+        super(CompositeField, self).contribute_to_class(cls, name)
+
+        # We can process the fields only after they've been added to the
+        # model class.
+        def process_enclosed_fields(sender, **kwargs):
+            nt_name = "%s_%s" % (cls.__name__, name)
+            nt_fields = " ".join(f.name for f in self.fields)
+            self.nt = get_composite_value_class(nt_name, nt_fields)
+
+        signals.class_prepared.connect(process_enclosed_fields,
+                                       sender=cls, weak=False)
+
+    def get_enclosed_fields(self):
+        return self.fields
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            raise AttributeError("%s can only be retrieved via instance."
+                                 % self.name)
+        return self.nt._make(getattr(instance, f.attname, None) for f in self.fields)
+
+    def __set__(self, instance, value):
+        # Ignore attempts to set to None; deletion code does that and we
+        # don't want to throw an exception.
+        if value is None:
+            return
+        value = self.to_python(value)
+        for f, val in zip([f.attname for f in self.fields], value):
+            setattr(instance, f, val)
+
+    def to_python(self, value):
+        if isinstance(value, six.string_types):
+            value = [unquote(v, escape=COMPOSITE_VALUE_QUOTING_CHAR)
+                     for v in value.split(COMPOSITE_VALUE_SEPARATOR)]
+
+        value = [f.to_python(v) for f, v in zip(self.fields, value)]
+        return value
+
+
+def get_composite_value_class(name, fields):
+    """
+    Returns a namedtuple subclass with our custom unicode representation.
+    """
+    nt = namedtuple(name, fields)
+
+    @python_2_unicode_compatible
+    class CompositeValue(nt):
+        def __str__(self):
+            return COMPOSITE_VALUE_SEPARATOR.join(
+                    quote(force_text(v),
+                          unsafe_chars=COMPOSITE_VALUE_SEPARATOR,
+                          escape=COMPOSITE_VALUE_QUOTING_CHAR)
+                    for v in self)
+
+    return CompositeValue
